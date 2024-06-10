a64377b984372969473fb0e71e4e41744699c3f2
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-10 18:02:40 -0400

    first commit

---
+++ hw_day1.py
@@ -0,0 +1,50 @@
+# 1. Showcase Your Dance Moves!
+
+# Taskt 1:
+# weather = "sunny"
+
+# if weather == "sunny":
+# print("Wear sunglasses!")
+# else:
+# print("Take an umbrella!")
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2
+
+if input() == "happy":
+    print("That's great to hear!")
+elif input() == "sad":
+    print("I hope your day gets better!")
+
+# 2. Python Data Types and type() Function
+
+# Task 1:
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+
+# 3. Arithmetic Operations in Daily Life
+
+# Task 1:
+
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+
+total = bread + peanut_butter + jelly
+
+print(total)
